bb7ac7545649f9d219d211619ce995d3572056a7
rainbowvs <527532711@qq.com>
2018-03-11 22:48:25 +0800

    新增2个订单页面

---
+++ src/components/page/Order/Order.vue
@@ -0,0 +1,83 @@
+<style lang="scss" scoped>
+	@import 'Order.scss';
+</style>
+
+<template>
+	<div class="order">
+		<my-header>
+			<i slot="left" class="yuewang icon-return" @click="goBack"></i>
+			<h1 slot="mid">我的订单</h1>
+		</my-header>
+		<div class="container">
+			<ul class="select">
+				<li :class="{active: active==0}" @click="active=0">全部</li>
+				<li :class="{active: active==1}" @click="active=1">待付款</li>
+				<li :class="{active: active==2}" @click="active=2">待发货</li>
+				<li :class="{active: active==3}" @click="active=3">待收货</li>
+				<li :class="{active: active==4}" @click="active=4">待评价</li>
+			</ul>
+			<ul class="content">
+				<li v-show="active==0">
+					<div class="top">
+						<span>越王珠宝自营店</span>
+						<em>待付款</em>
+					</div>
+					<div class="mid">
+						<div class="left">
+							<img src="https://cdn.ctfmall.com/thumb/F156901.jpg"/>
+						</div>
+						<div class="right">
+							<p>车花心形黄金足金戒指车花心形黄金足金戒指车花心形黄金足金戒指车花心形黄金足金戒指</p>
+							<p>共{{1}}件商品</p>
+							<p>
+								合计<em>￥760.00</em>
+							</p>
+						</div>
+					</div>
+					<div class="bottom">
+						<div class="left">
+							<a href="javascript:;" @click="detailClick">订单详情</a>
+							<a href="javascript:;">付款</a>
+						</div>
+						<div class="right">
+							<a href="javascript:;">删除订单</a>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+	import Header from '@/components/module/Header/Header';
+	export default {
+		data () {
+			return {
+				alls: [
+					{}
+				],
+				pays: [],
+				deliveries: [],
+				receives: [],
+				evaluates: [],
+				active: 0,
+			}
+		},
+		created () {
+			
+		},
+		methods: {
+			detailClick () {
+				let that = this;
+				that.$router.push({name: 'OrderDetail'});
+			},
+			goBack () {
+				this.$router.push({name: 'User'});
+			},
+		},
+		components: {
+			'my-header': Header,
+		}
+	}
+</script>
---
+++ src/components/page/OrderDetail/OrderDetail.vue
@@ -0,0 +1,113 @@
+<style lang="scss" scoped>
+	@import 'OrderDetail.scss';
+</style>
+
+<template>
+	<div class="orderdetail">
+		<my-header>
+			<i slot="left" class="yuewang icon-return" @click="goBack"></i>
+			<h1 slot="mid">订单详情</h1>
+		</my-header>
+		<div class="container">
+			<div class="address">
+				<div class="left">
+					<i class="yuewang icon-location"></i>
+				</div>
+				<div class="right">
+					<p>
+						<span>收货人：张三</span>
+						<em>15099976289</em>
+					</p>
+					<p>收货地址：广州市白云区松洲街广州市白云区松洲街广州市白云区松洲街广州市白云区松洲街广州市白云区松洲街</p>
+				</div>
+			</div>
+			<div class="shopInfo">
+				<ul>
+					<li>
+						<span>名称: </span>
+						<em>猴赛雷黄金足金吊坠</em>
+					</li>
+					<li>
+						<span>规格: </span>
+						<em>材质黄金</em>
+					</li>
+					<li>
+						<span>数量: </span>
+						<em>20</em>
+					</li>
+					<li>
+						<span>金额: </span>
+						<em>￥20</em>
+					</li>
+				</ul>
+			</div>
+			<div class="otherInfo">
+				<ul>
+					<li>
+						<span>订单编号: </span>
+						<em>180311132278</em>
+					</li>
+					<li>
+						<span>订单状态: </span>
+						<em>待付款</em>
+					</li>
+					<li>
+						<span>配送方式: </span>
+						<em>快递</em>
+					</li>
+					<li>
+						<span>支付方式: </span>
+						<em>在线支付</em>
+					</li>
+					<li>
+						<span>买家留言: </span>
+						<em>无</em>
+					</li>
+					<li>
+						<span>下单时间: </span>
+						<em>2018-03-11 15:02:18</em>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="footer">
+			<div class="left">
+				<span>合计：</span>
+				<em>￥20</em>
+			</div>
+			<div class="right">
+				立即支付
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import Header from '@/components/module/Header/Header';
+	export default {
+		data () {
+			return {
+				
+			}
+		},
+		created () {
+			
+		},
+		methods: {
+			AddressClick () {
+				let that = this;
+				that.$store.commit('SET_ADDRESSBACKNAME',{
+					name: 'OrderConfirm',
+				});
+				that.$router.push({name: 'Address'});
+			},
+			goBack () {
+				let that = this;
+				that.$router.push({name: 'Order'});
+			},
+		},
+		components: {
+			'my-header': Header,
+		}
+	}
+</script>
