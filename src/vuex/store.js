d8afaaec6ecd235ae5810606b92dcf6b05753462
rainbowvs <527532711@qq.com>
2018-03-17 00:46:07 +0800

    新增购物车页面交互的方法

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,4 +1,4 @@
-import Vue from 'vue';
+﻿import Vue from 'vue';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
@@ -18,6 +18,7 @@ const state = {
 	userInfo: JSON.parse(window.localStorage.getItem('userInfo')),//用户信息
 	AddressBackName: '',//收货地址管理页面返回页面名字
 	OrderConfirmBackName: '',//订单确认页面返回页面名字
+	shopCart: window.localStorage['shopCart'] ? JSON.parse(window.localStorage['shopCart']) : [],
 };
 
 const mutations = {
@@ -54,9 +55,59 @@ const mutations = {
 		//设定订单确认返回
 		state.OrderConfirmBackName = obj.name;
 	},
+	ADD_SHOPCART (state,obj) {
+		//购物车增加商品
+		if(state.shopCart.length == 0)
+			state.shopCart.push(obj);
+		else{
+			let flag = false;
+			for(let i=0,len=state.shopCart.length;i<len;i++){
+				if(state.shopCart[i]['id'] == obj['id']){
+					if(state.shopCart[i]['size'] == obj['size']){
+						flag = true;
+						state.shopCart[i]['amount'] += obj['amount'];
+						break;
+					}
+				}
+				if((!flag) && (i==len-1))
+					state.shopCart.push(obj);
+			}
+		}
+
+		state.shopCart.sort((a,b) => {
+			return a['id'] - b['id'];
+		});
+		
+		window.localStorage.setItem('shopCart',JSON.stringify(state.shopCart));
+	},
+	DELETE_SHOPCART (state,obj) {
+		//购物车删除商品
+		state.shopCart.splice(obj.index,1);
+		window.localStorage.setItem('shopCart',JSON.stringify(state.shopCart));
+	},
+	UPDATE_SHOPCART (state,obj) {
+		//购物车更新商品
+		state.shopCart.forEach((v,i) => {
+			if(i == obj.index)
+				for(let key in obj)
+					v['key'] = obj['key'];
+		});
+		window.localStorage.setItem('shopCart',JSON.stringify(state.shopCart));
+	},
+};
+const getters = {
+	sum : state => {
+		let totalPrice = 0;
+        state.shopCart.forEach((item) => {
+            if(item.isChecked)
+                totalPrice += item.price * item.amount
+        });
+        return totalPrice;
+	},
 };
 
 export default new Vuex.Store({
 	state,
-	mutations
+	mutations,
+	getters,
 });
