88da2986abb365150004b7b7fab45aa352c29e90
rainbowvs <527532711@qq.com>
2018-03-20 00:21:26 +0800

    修改部分结构 添加收货地址交互

--- src/components/page/AddressEdit/AddressEdit.vue
+++ src/components/page/AddressEdit/AddressEdit.vue
@@ -22,9 +22,18 @@
 					<label for="address">详细地址：</label>
 					<input type="text" id="address" v-model="addressInfo.address" />
 				</li>
+				<li v-if="acquiescent_show" @click="addressInfo.acquiescent=!addressInfo.acquiescent" :class="{acquiescent: addressInfo.acquiescent}">
+					<template v-if="addressInfo.acquiescent">
+						<i class="yuewang icon-radio-checked"></i>
+					</template>
+					<template v-else>
+						<i class="yuewang icon-radio"></i>
+					</template>
+					<span>设为默认</span>
+				</li>
 			</ul>
 		</div>
-		<div class="footer">
+		<div class="footer" @click="saveClick">
 			<a href="javascript:;">保存</a>
 		</div>
 	</div>
@@ -36,12 +45,57 @@
 		data () {
 			return {
 				addressInfo: null,
+				acquiescent_show: false,
 			}
 		},
 		created () {
 			this.addressInfo = this.$route.params.address;
+			this.acquiescent_show = this.$route.params.show;
 		},
 		methods: {
+			saveClick () {
+				let that = this;
+				that.$ajax({
+					name: '编辑收货地址',
+					url: window.reqUrl + 'address.php',
+					data: {
+						handle: 'edit',
+						uid: that.$store.state.userInfo['id'],
+						token: that.$store.state.userInfo['token'],
+						id: that.addressInfo.id,
+						name: that.addressInfo.name,
+						phone: that.addressInfo.phone,
+						address: that.addressInfo.address,
+						acquiescent: that.addressInfo.acquiescent,
+					},
+					beforeSend () {
+						that.$store.commit('SHOW_LOADING');
+					}
+				}).then(res => {
+					if(res.type == 'success'){
+						that.$store.commit('SHOW_TOAST',{
+							text: res.msg
+						});
+						setTimeout(() => {
+							that.$router.push({name: 'Address'});
+						},1000);
+					}else{
+						if(res.status == 1)
+							setTimeout(() => {
+								that.$router.push({name: 'Login'});
+							},1000);
+						that.$store.commit('SHOW_TOAST',{
+							text: res.msg
+						});
+					}
+					that.$store.commit('HIDE_LOADING');
+				}).catch(status => {
+					that.$store.commit('HIDE_LOADING');
+					that.$store.commit('SHOW_TOAST',{
+						text: status
+					});
+				});
+			},
 			goBack () {
 				this.$router.push({name: 'Address'});
 			},
