db6e39cb5e69335c001b9d0ab26698bcc3ece02e
rainbowvs <527532711@qq.com>
2018-03-21 16:32:58 +0800

    部分页面添加路由拦截

--- src/router/index.js
+++ src/router/index.js
@@ -30,7 +30,7 @@ const router = new Router({
 	    		name: 'User',
 	    		component: () => import('@/components/page/User/User.vue'),
 	    		meta: {
-			    	requireProp: true,//需要对跳转进行验证,详情在main.js
+			    	requireProp: true,//需要对跳转进行验证
 			    },
 	    	}
 	    ],
@@ -44,39 +44,57 @@ const router = new Router({
 	  	path: '/app/order',
 	  	name: 'Order',
 	  	component: () => import('@/components/page/Order/Order.vue'),
+	  	meta: {
+	    	requireProp: true,//需要对跳转进行验证
+	    },
 	  },
 	  {
-	  	path: '/app/orderconfirm',
+	  	path: '/app/order/orderconfirm',
 	  	name: 'OrderConfirm',
 	  	component: () => import('@/components/page/OrderConfirm/OrderConfirm.vue'),
+	  	meta: {
+	    	requireProp: true,//需要对跳转进行验证
+	    },
 	  },
 	  {
 	  	path: '/app/order/orderdetail',
 	  	name: 'OrderDetail',
 	  	component: () => import('@/components/page/OrderDetail/OrderDetail.vue'),
+	  	meta: {
+	    	requireProp: true,//需要对跳转进行验证
+	    },
 	  },
 	  {
-	  	path: '/app/orderconfirm/address',
+	  	path: '/app/order/orderconfirm/address',
 	  	name: 'Address',
 	  	component: () => import('@/components/page/Address/Address.vue'),
+	  	meta: {
+	    	requireProp: true,//需要对跳转进行验证
+	    },
 	  },
 	  {
-	  	path: '/app/orderconfirm/address/addressedit',
+	  	path: '/app/order/orderconfirm/address/addressedit',
 	  	name: 'AddressEdit',
 	  	component: () => import('@/components/page/AddressEdit/AddressEdit.vue'),
+	  	meta: {
+	    	requireProp: true,//需要对跳转进行验证
+	    },
 	  },
 	  {
-	  	path: '/app/orderconfirm/address/addressadd',
+	  	path: '/app/order/orderconfirm/address/addressadd',
 	  	name: 'AddressAdd',
 	  	component: () => import('@/components/page/AddressAdd/AddressAdd.vue'),
+	  	meta: {
+	    	requireProp: true,//需要对跳转进行验证
+	    },
 	  },
 	  {
-	  	path: '/app/login',
+	  	path: '/app/user/login',
 	  	name: 'Login',
 	  	component: () => import('@/components/page/Login/Login.vue'),
 	  },
     {
-      path: '/app/login/register',
+      path: '/app/user/login/register',
       name: 'Register',
       component: () => import('@/components/page/Register/Register.vue'),
     },
