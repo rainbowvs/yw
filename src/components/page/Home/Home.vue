87b98085e203690dbf40ec76d3a4bfbec3c18818
rainbowvs <527532711@qq.com>
2018-03-22 21:23:59 +0800

    暂时去掉部分静态区域

--- src/components/page/Home/Home.vue
+++ src/components/page/Home/Home.vue
@@ -5,12 +5,8 @@
 <template>
 	<div class="home">
 		<my-header>
-			<h1 slot="left">越王</h1>
-			<form slot="mid">
-				<input type="text" placeholder="戒指" readonly @click="$router.push({name: 'Categories'})" />
-				<i class="yuewang icon-search"></i>
-			</form>
-			<i slot="right" class="yuewang icon-message"></i>
+			<i class="yuewang icon-ring" slot="left"></i>
+			<h1 slot="mid">越王珠宝移动商城</h1>
 		</my-header>
 		<div class="container">
 			<div class="swiper-container">
@@ -21,67 +17,21 @@
 			    </div>
 			    <div class="swiper-pagination"></div>
 			</div>
-			<div class="nav">
-				<ul>
-					<li>
-						<a href="javascript:void(0);">
-							<img src="../../../../static/img/activity.png"/>
-							<span>活动</span>
-						</a>
-					</li>
-					<li>
-						<a href="javascript:void(0);">
-							<img src="../../../../static/img/rank.png"/>
-							<span>排行榜</span>
-						</a>
-					</li>
-					<li>
-						<a href="javascript:void(0);">
-							<img src="../../../../static/img/shopbag.png"/>
-							<span>购物袋</span>
-						</a>
-					</li>
-					<li>
-						<a href="javascript:void(0);">
-							<img src="../../../../static/img/coupon.png"/>
-							<span>优惠券</span>
-						</a>
-					</li>
-				</ul>
-			</div>
 			<img src="../../../../static/img/show.jpg"/>
 			<div class="kind">
 				<ul>
-					<li>
+					<li @click="$router.push({name: 'Categories'})">
 						<a href="javascript:void(0);">
 							<img src="../../../../static/img/hot_pro.jpg"/>
 						</a>
 						<h6>热门产品</h6>
 					</li>
-					<li>
+					<li @click="$router.push({name: 'Categories'})">
 						<a href="javascript:void(0);">
 							<img src="../../../../static/img/latest_pro.jpg"/>
 						</a>
 						<h6>新品上架</h6>
-					</li>
-					<li>
-						<a href="javascript:void(0);">
-							<img src="../../../../static/img/popular_pro.jpg"/>
-						</a>
-						<h6>爆款推荐</h6>
-					</li>
-					<li>
-						<a href="javascript:void(0);">
-							<img src="../../../../static/img/hot_pro.jpg"/>
-						</a>
-						<h6>热门产品</h6>
-					</li>
-					<li>
-						<a href="javascript:void(0);">
-							<img src="../../../../static/img/latest_pro.jpg"/>
-						</a>
-						<h6>新品上架</h6>
-					</li>
+					</li @click="$router.push({name: 'Categories'})">
 					<li>
 						<a href="javascript:void(0);">
 							<img src="../../../../static/img/popular_pro.jpg"/>
