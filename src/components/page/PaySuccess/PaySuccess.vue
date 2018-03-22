ce15659508f758205069d926ecd8ca645023b32b
rainbowvs <527532711@qq.com>
2018-03-22 21:15:43 +0800

    添加支付成功页面

---
+++ src/components/page/PaySuccess/PaySuccess.vue
@@ -0,0 +1,37 @@
+<style lang="scss" scoped>
+	@import 'PaySuccess.scss';
+</style>
+
+<template>
+	<div class="paysuccess">
+		<my-header>
+			<i slot="left"></i>
+			<h1 slot="mid">支付成功</h1>
+		</my-header>
+		<div class="container">
+			<div class="info">
+				<i class="yuewang icon-roundcheck"></i>
+				<span>支付成功</span>
+				<a href="javascript:;" @click="$router.push({name: 'Order'})">返回订单页面</a>
+				<a href="javascript:;" @click="$router.push({name: 'Categories'})">继续购物</a>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import Header from '@/components/module/Header/Header';
+	export default {
+		data () {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		},
+		components: {
+			'my-header': Header,
+		}
+	}
+</script>
